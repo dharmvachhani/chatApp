73aa35cdd6c5fd7c539150e24ca8ae9d08de22e8
Dharm Vachhani <dharmvachhani256@gmail.com>
2021-05-28 22:47:40 +0530

    update

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -2,14 +2,23 @@ var User = require("../models/usersModel");
 
 module.exports = function (req, res, next) {
   console.log("profile");
-  User.findOne({ unique_id: req.session.userid }, function (err, data) {
-    console.log("data");
-    console.log(data);
-    if (!data) {
-      res.redirect("/");
-    } else {
-      //console.log("found");
-      return res.render("data", { name: data.username, email: data.email });
-    }
+  User.find(function (err, result) {
+    if (err) throw err;
+    const alluser = result;
+    console.log(alluser);
+
+    User.findOne({ unique_id: req.session.userid }, function (err, data) {
+      console.log("data");
+      console.log(data);
+      if (!data) {
+        res.redirect("/");
+      } else {
+        //console.log("found");
+        return res.render("data", {
+          user: data,
+          alluser: alluser,
+        });
+      }
+    });
   });
 };
